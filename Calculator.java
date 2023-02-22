01a14a7b640e3d8c1488798159bd329d5ad4241b
shadaban <125877490+shadaban@users.noreply.github.com>
2023-02-22 10:47:41 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+class Calculator{
+  int a;
+  int b;
+  int plus(int a,int b){
+      int c=a+b;
+    System.out.println(c);
+  }
+  int minus(int a,int b){
+      int c=a-b;
+      System.out.println(c);
+  }
+  int multiplication(int a,int b){
+      int c=a*b;
+      System.out.println(c);
+  }
+  int division(int a,int b){
+      int c=a/b;
+    System.out.println(c);
+  }
+}
+public static void main(String args[]){
+    int a=10,b=5;
+    Calculator cl=new Calculator();
+  cl.plus(a,b);
+  cl.minus(a,b);
+  cl.multiplication(a,b);
+  cl.division(a,b);
+}
