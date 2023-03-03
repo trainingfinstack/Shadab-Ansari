9d0897f59aadae36b7604a5d1fe5932df60feb9e
shadaban <125877490+shadaban@users.noreply.github.com>
2023-03-03 09:51:49 +0530

    Update BookController.java

--- CrudWithDB/src/main/java/com/example/Controller/BookController.java
+++ CrudWithDB/src/main/java/com/example/Controller/BookController.java
@@ -1,7 +1,8 @@
 package com.example.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
+import java.util.Optional;
 import com.example.models.Book;
 import com.example.repo.BookRepo;
 @RestController
@@ -12,5 +13,22 @@ public class BookController {
 	public String insertBook(@RequestBody Book book) {
 		bookRepository.save(book);
 		return "Your record is saved successfully !!";
+	}
+	@PostMapping("/multipleBookSave")
+	public String insertBook(@RequestBody List<Book> book) {
+		bookRepository.saveAll(book);
+		return "Your record is saved successfully !!";
+	}
+	@GetMapping("/getAllBook")
+	public List<Book> getBook(){ 
+		return (List<Book>) bookRepository.findAll();
+	}
+	@GetMapping("getByBookName/{name}")
+	public List<Book> getBook(@PathVariable("name") String bookName){
+		return (List<Book>) bookRepository.findBybookName(bookName);
+	}
+	@GetMapping("/getByBookId/{bookId}")
+	public Optional<Book> getBookId(@PathVariable("bookId") Long id) {
+		return bookRepository.findById(id);
 	}
 }
